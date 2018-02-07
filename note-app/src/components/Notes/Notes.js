3e823dd1b70e33715a359bf8d756dc56f6783ca6
Samuel Cha <sunchipssam@gmail.com>
2018-02-07 11:32:36 -0800

    Seeing single note view working but bugs
    Able to properly view and edit single notes, but deleting notes has some bugs.
    
    files modified:
    Notes.js

--- note-app/src/components/Notes/Notes.js
+++ note-app/src/components/Notes/Notes.js
@@ -7,22 +7,46 @@ import '../../styles/css/index.css';
 class Notes extends Component {
 	state = {
 		notes: [],
+		displayNotes: [],
 	};
 
 	componentDidMount() {
-		this.setState({ notes: this.props.notes });
+		this.setState({ notes: this.props.notes, displayNotes: this.props.notes });
 	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.notes.length !== this.props.notes)
+			this.setState({ notes: nextProps.notes });
+	}
+
+	detailedNoteView = note => {
+		this.setState({ displayNotes: [note] });
+	};
+
+	ReturnToAllNotes = _ => {
+		this.setState({ displayNotes: [...this.state.notes] });
+	};
 
 	render() {
 		return (
 			<div className="Notes">
-				{this.state.notes.map(note => {
+				{this.state.displayNotes.map(note => {
 					return (
-						<div key={note.id} className="NoteContainer">
+						<div
+							key={note.id}
+							className="NoteContainer"
+							onClick={_ => this.detailedNoteView(note)}
+						>
 							<Note note={note} />
 						</div>
 					);
 				})}
+
+				{this.state.displayNotes.length > 1 ? null : (
+					<div className="NotesReturnButton" onClick={this.ReturnToAllNotes}>
+						return
+					</div>
+				)}
 			</div>
 		);
 	}
